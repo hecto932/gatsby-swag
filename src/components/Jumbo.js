27e0eed3364b2cb627f7722cea5d0bb3d3d4b5d7
Hector Flores <hflores@nearsoft.comm>
2019-07-04 19:21:16 -0700

    Using react

---
+++ src/components/Jumbo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyledJumbo } from '../styles/components'
+
+export default function Jumbo() {
+  return (
+    <StyledJumbo>
+      <div>
+        <h2>Consigue el mejor swag exclusivo y especial de Platzi!</h2>
+        <small>Please</small>
+      </div>
+    </StyledJumbo>
+  )
+}
